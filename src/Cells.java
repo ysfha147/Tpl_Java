cd5752936fd66baa27cd553b68c36815014dd5ef
Youdssef Hannat <Youdssef.Hannat@grenoble-inp.org>
2023-11-09 13:27:07 +0100

    the implementation of next() in CellsSimulator

--- src/Cells.java
+++ src/Cells.java
@@ -1,10 +1,13 @@
+import gui.GUISimulator;
 import gui.Simulable;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Cells {
+    /**/
     private Boolean[][] cells_matrix;
     private int height; private int width;
 
@@ -33,6 +36,18 @@ public class Cells {
         }
     }
 
+    public Boolean[][] getCells_matrix() {
+        return cells_matrix;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Boolean[] line: cells_matrix){
@@ -53,6 +68,56 @@ public class Cells {
 
 class CellsSimulator implements Simulable{
 
+    private Cells cells;
+    private GUISimulator gui;
+    private Color cellsColor;
+    public CellsSimulator(Cells cells, GUISimulator gui, Color cellsColor){
+        this.gui = gui;
+        gui.setSimulable(this);
+        this.cellsColor = cellsColor;
+        this.cells = cells;
+        draw();
+    }
+
+    /**
+     *
+     * @param i
+     * @param j
+     * @return an iterator over the indexes of the neighbors of the element in of
+     * the index (i,j) of the matrix of size n*m
+     */
+    private Iterator<Point> NeighborIterator(int i, int j){
+        List<Point> neighbors = new ArrayList<>();
+        int n = cells.getHeight(); int m = cells.getWidth();
+        if (i == 1){
+            if (j == 1){
+                neighbors.add(new Point(i,j+1));//right
+                neighbors.add(new Point(i+1, j+1));//right down
+                neighbors.add(new Point(n,j+1));//right up
+                neighbors.add(new Point(i,m)); //left
+                neighbors.add(new Point(i+1, m));//left down
+                neighbors.add(new Point(n, m));//left up
+                neighbors.add(new Point(i+1, j));//down
+                neighbors.add(new Point(n,j));//up
+            }
+        }
+
+        return neighbors.iterator();
+    }
+
+
+    /**
+     * returns true if the cell in the row i and column j is can be set Alive
+     * in the next step and false if not
+     * @param i
+     * @param j
+     * @param Cells_matrix
+     * @return
+     */
+    private boolean estSetAlive(int i, int j, Boolean[][] Cells_matrix){
+        return true;
+    }
+
     @Override
     public void next() {
 
@@ -60,6 +125,10 @@ class CellsSimulator implements Simulable{
 
     @Override
     public void restart() {
+
+    }
+
+    private void draw(){
 
     }
 
